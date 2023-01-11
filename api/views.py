06ab7c30159627905cdc0fd80a3b35b952cf036f
Atyanta Awesa <awesaatyanta.p@gmail.com>
2023-01-12 00:08:20 +0700

    feat: visualization done

--- api/views.py
+++ api/views.py
@@ -22,44 +22,45 @@ def update_ispu(request):
     if request.method == 'POST':
         json_data = json.loads(request.body)
         try:
-            if json_data['api_key'] == os.environ['API_KEY']:
-
-                ispu = ISPU.objects.all()
-
-                now = datetime.now(pytz.utc)
-
-                if len(ispu) > 0:
-                    ispu_timestamp = ispu[0].timestamp
-                    if ispu_timestamp.date() == now.date() and ispu_timestamp.hour == now.hour:
-                        msg = f'update not performed since ispu data already exists for {utc_to_local(ispu_timestamp)}'
-                        return JsonResponse({
-                            'status': 'false',
-                            'message': msg
-                        })
-
-
-                timestamps = Timestamp.objects.filter(timestamp__range=[now - timedelta(hours=24), now.replace(minute=0, second=0, microsecond=0)])
-                pm25 = []
-                for timestamp in timestamps:
-                    data = Pollutant.objects.filter(timestamp=timestamp).select_related().values_list('pm25')
-                    pm25.append(data.aggregate(Avg('pm25'))['pm25__avg'])
-                
-                new_ispu_value = calculate_ispu(pm25)
-
-                ISPU.objects.all().delete()
-
-                new_ispu = ISPU(value=new_ispu_value)
-                new_ispu.save()
-
-                return JsonResponse({
-                    'status': 'true',
-                    'message': str(new_ispu)
-                })
-            else:
-                return JsonResponse(status=403, data={'status': 'false', 'message': 'invalid API key'})
-        except Exception as e:
-            traceback.print_exc()
-
+            _ = json_data['api_key'] == os.environ['API_KEY']
+        except:
             return JsonResponse(status=400, data={'status': 'false', 'message': 'malformed request body'})
+        if json_data['api_key'] == os.environ['API_KEY']:
+
+            ispu = ISPU.objects.all()
+
+            now = datetime.now(pytz.utc).replace(minute=1, second=0, microsecond=0)
+
+            if len(ispu) > 0:
+                ispu_timestamp = ispu[0].timestamp
+                if ispu_timestamp.date() == now.date() and ispu_timestamp.hour == now.hour:
+                    msg = f'update not performed since ispu data already exists for {utc_to_local(ispu_timestamp)}'
+                    return JsonResponse({
+                        'status': 'false',
+                        'message': msg
+                    })
+
+
+            timestamps = Timestamp.objects.filter(timestamp__range=[now - timedelta(hours=24), now]).order_by('timestamp')
+            pm25 = []
+            for timestamp in timestamps:
+                data = Pollutant.objects.filter(timestamp=timestamp).select_related().values_list('pm25')
+                pm25.append(data.aggregate(Avg('pm25'))['pm25__avg'])
+                
+            new_ispu_value = calculate_ispu(pm25)
+
+            ISPU.objects.all().delete()
+
+            new_ispu = ISPU(value=new_ispu_value)
+            new_ispu.save()
+
+            print(new_ispu)
+
+            return JsonResponse({
+                'status': 'true',
+                'message': str(new_ispu)
+            })
+        else:
+            return JsonResponse(status=403, data={'status': 'false', 'message': 'invalid API key'})
     else:
         return JsonResponse(status=405, data={'status': 'false', 'message': 'method not allowed'})
